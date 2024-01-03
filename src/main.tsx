e2c80b43e5ba830f117074ff7cfd7ce530a4d693
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-03 12:59:04 +0200

    Add and connect redux store to the project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// RTK
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
 // Components
 import App from "./app/App.tsx";
 
@@ -10,6 +14,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 );
