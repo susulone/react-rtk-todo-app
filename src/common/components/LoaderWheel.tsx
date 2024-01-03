d0fcc021944e74b3659e79d98fd3e10918480e4b
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-03 13:35:46 +0200

    Add LoaderWheel component to the TaskList.

---
+++ src/common/components/LoaderWheel.tsx
@@ -0,0 +1,7 @@
+// Styling
+import "./styles.css";
+import { Loader } from "react-feather";
+
+export const LoaderWheel = () => {
+  return <Loader id="loader" size={48} />;
+};
--- src/features/tasks/TaskList.tsx
+++ src/features/tasks/TaskList.tsx
@@ -1,9 +1,13 @@
+// Components
+import { LoaderWheel } from '../../common/components/LoaderWheel'
+
 // Styling
 import "./styles.css";
 
 export const TaskList = () => {
   return (
     <main id="task-list">
+      <LoaderWheel />
     </main>
   );
 };
