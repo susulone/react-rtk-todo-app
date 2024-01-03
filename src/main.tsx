68e57cb6869b730f20d0b007ae7f84705732c3e0
Suvi Sulonen <suvi.sulonen@gmail.com>
2024-01-03 12:40:11 +0200

    Set up Bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// Components
 import App from './app/App.tsx'
+
+// Styling
+import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
